9d575257cc3c6e741ce1f5e294e84f6a2e6a36d7
smahesh-kumarr <maheshkumar08042006@gmail.com>
2025-03-12 15:03:38 +0530

    Update LoginPage.js

--- client/src/Pages/LoginPage.js
+++ client/src/Pages/LoginPage.js
@@ -16,7 +16,7 @@ const LoginPage = () => {
         setError('');
 
         try {
-            const response = await fetch('http://localhost:5000/login', {
+            const response = await fetch('http://localhost:4000/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
